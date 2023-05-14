4f610a026dfce5abcc16277e994a44278af52a62
KauLeal <kauleal44@gmail.com>
2023-05-14 15:43:20 -0300

    Update funcao-de-calculo.ts

--- exemplos/exemplos de teste/funcao-de-calculo.ts
+++ exemplos/exemplos de teste/funcao-de-calculo.ts
@@ -1,21 +1,24 @@
-// Neste exemplo, a função calcular recebe dois valores do tipo number e uma string que representa a operação a ser realizada. Através da estrutura de controle switch, a função realiza a operação desejada e retorna o resultado. Caso a operação passada como parâmetro não seja uma das opções válidas, a função lança uma exceção através do comando throw.
+// Neste exemplo, a função calcular recebe dois valores do tipo number e uma string que representa a operação a ser realizada.
+/// Através da estrutura de controle switch, a função realiza a operação desejada e retorna o resultado.
+/// Caso a operação passada como parâmetro não seja uma das opções válidas, a função lança uma exceção através do comando throw.
 
-function calcular(valor1: number, valor2: number, operacao: string): number {
-    switch (operacao) {
-      case "soma":
-        return valor1 + valor2;
-      case "subtracao":
-        return valor1 - valor2;
-      case "multiplicacao":
-        return valor1 * valor2;
-      case "divisao":
-        return valor1 / valor2;
-      default:
-        throw new Error("Operação inválida!");
-    }
-  }
+function calcular(): number {
+  const valor1 = Number(prompt("Digite o primeiro valor: "));
+  const valor2 = Number(prompt("Digite o segundo valor: "));
+  const operacao = prompt("Digite a operação desejada (soma, sub, mult ou div): ");
   
-  console.log(calcular(5, 3, "soma")); // Resultado: 8
-  console.log(calcular(10, 4, "subtracao")); // Resultado:6
-  console.log(calcular(7, 6, "multiplicacao")); // Resultado: 42
-  console.log(calcular(100, 3, "divisao")); // Resultado: 33,33333333333333
+  switch (operacao) {
+    case "soma":
+      return valor1 + valor2;
+    case "sub":
+      return valor1 - valor2;
+    case "mult":
+      return valor1 * valor2;
+    case "div":
+      return valor1 / valor2;
+    default:
+      throw new Error("Operação inválida!");
+  }
+}
+
+console.log(calcular());
