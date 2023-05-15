02202b75428898eeae3f0df8bb0dd254ae43d32d
KauLeal <kauleal44@gmail.com>
2023-05-15 13:35:43 -0300

    Update imprimir-dados.ts

--- exemplos/exemplos de teste/imprimir-dados.ts
+++ exemplos/exemplos de teste/imprimir-dados.ts
@@ -16,15 +16,15 @@ function imprimirDados(pessoa: Pessoa) {
 }
 
 while (true) {
-  const resposta = prompt("Deseja inserir uma pessoa? (S = sim / N = Não)"); // Pergunta ao usuário se ele quer inserir uma pessoa
-  if (!resposta || resposta.toLowerCase() === "n") { // O ! verifica se resposta é nula ou indefinida. Caso seja,
-    break;                                           // ou o usuário digite "n", o código pára.
-  } else if (resposta.toLowerCase() !== "s") {       // Caso o usuário digite "s", o código continua.
+  const resposta = prompt("Deseja inserir uma pessoa? (S = sim / N = Não)");
+  if (!resposta || resposta.toLowerCase() === "n") { // O ! verifica se resposta é nula ou indefinida
+    break;                                           
+  } else if (resposta.toLowerCase() !== "s") {  
     continue;
   }
 
-  const nome = prompt("Digite o nome da pessoa:");   // Cria a constante nome a partir do valor digitado pelo usuário.
-  if (!nome) {                                       // Checa se nome é nula ou indefinida
+  const nome = prompt("Digite o nome da pessoa:"); 
+  if (!nome) {                                      
     continue;
   }
 
@@ -32,7 +32,7 @@ while (true) {
   while (true) {
     const idadeStr = prompt("Digite a idade da pessoa:");
     idade = parseInt(idadeStr || "0", 10);           // Transforma idade de string para inteiro utilizando a base 10.
-    if (isNaN(idade)) {                              // Caso o usuário digite algo diferente de um inteiro,o loop reinicia.
+    if (isNaN(idade)) {                              // Caso o usuário digite algo diferente de um número,o loop reinicia.
       continue;                                      // Caso clique em "cancelar", idade será exibida como "0"
     }
     break;
@@ -41,6 +41,6 @@ while (true) {
   const cidade = prompt("Digite a cidade da pessoa:") || "Desconhecida"; // Caso o usuário clique em "cancelar",
                                                                          // a cidade será exibida como "desconhecida".
 
-  const pessoa: Pessoa = {nome, idade, cidade};      // Cria o objeto pessoa a partir das constantes obtidas
-  imprimirDados(pessoa);                             // Chama a função ImprimirDados
+  const pessoa: Pessoa = {nome, idade, cidade};      
+  imprimirDados(pessoa);                            
 }
